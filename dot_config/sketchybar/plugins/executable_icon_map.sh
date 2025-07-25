09cfb3aa7815fa41d3dc0cd35358148ba2f92d34
Liu Xiaochen <xiaochen.liu01@g.softbank.co.jp>
2025-07-26 01:10:56 +0900

    minor fix

--- dot_config/sketchybar/plugins/executable_icon_map.sh
+++ dot_config/sketchybar/plugins/executable_icon_map.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 ### START-OF-ICON-MAP
 function __icon_map() {
--- dot_config/sketchybar/plugins/executable_space.sh
+++ dot_config/sketchybar/plugins/executable_space.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 if [ "$1" = "$FOCUSED_WORKSPACE" ]; then
     sketchybar --set $NAME background.drawing=on
