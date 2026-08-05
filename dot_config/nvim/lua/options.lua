b071568a83775298544e20053edee06248ac68b0
gzfrozen <gzliuxiaochen@gmail.com>
2026-08-05 12:05:20 +0900

    minor change of nvim config

--- dot_config/nvim/lua/options.lua
+++ dot_config/nvim/lua/options.lua
@@ -39,6 +39,11 @@ vim.filetype.add({
   },
 })
 
+-- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
+-- instead raise a dialog asking if you wish to save the current file(s)
+-- See `:help 'confirm'`
+vim.o.confirm = true
+
 -- For Auto-Session
 vim.opt.sessionoptions = "buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
 
--- dot_config/nvim/lua/plugins/not_vscode/treesitter.lua
+++ dot_config/nvim/lua/plugins/not_vscode/treesitter.lua
@@ -58,8 +58,9 @@ return {
 
       -- enables treesitter based folds
       -- for more info on folds see `:help folds`
-      -- vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
-      -- vim.wo.foldmethod = 'expr'
+      vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+      vim.wo.foldmethod = "expr"
+      vim.opt.foldenable = false
 
       -- check if treesitter indentation is available for this language, and if so enable it
       -- in case there is no indent query, the indentexpr will fallback to the vim's built in one
